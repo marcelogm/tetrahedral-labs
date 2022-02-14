7e114840606ac58e0c4d3b0c7845ee4f56982421
Marcelo Gomes Martins <marcelo-martins@defensoria.rs.def.br>
2022-02-14 18:36:59 -0300

    feat: .obj to .node

--- create_tetrahedral_mesh.py
+++ create_tetrahedral_mesh.py
@@ -1,1 +1,13 @@
+import pyvista as pv
 import tetgen
+import numpy as np
+import meshio
+
+sphere = pv.read('objects/pipe_12v.obj')
+tet = tetgen.TetGen(sphere)
+tet.tetrahedralize(order=1, mindihedral=2, minratio=1.5)
+grid = tet.grid
+grid.save('pipe_12v.vtk')
+
+mesh = meshio.read('pipe_12v.vtk')
+meshio.write('pipe_12v.node', mesh)
