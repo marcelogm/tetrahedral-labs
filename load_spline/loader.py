74116a1667decaf495bc7351d50bff79271bb265
Marcelo Gomes Martins <marcelo-martins@defensoria.rs.def.br>
2022-03-28 22:03:54 -0300

    Draft: feat: create a cube

--- load_spline/loader.py
+++ load_spline/loader.py
@@ -1,4 +1,5 @@
 from io import FileIO
+from tkinter import E
 import plotly.graph_objects as go
 import numpy as np
 
@@ -47,6 +48,56 @@ class SplineLoader:
         return np.array([ coord[0], coord[1], coord[2] ])
 
 
+#    E ----- F  ← (i + 1)
+#  / |     / |
+# A ----- B  |  ← (i + 1)
+# |  G ---|- H  ← i
+# | /     | /
+# C ----- D     ← i
+# 
+# ↑       ↑
+# l       r
+def extract_cuboid_index(i, l, r):
+    A =  (i * 64)       + (l * 2) + 0
+    B =  (i * 64)       + (r * 2) + 2
+    C = ((i + 1) * 64)  + (l * 2) + 0
+    D = ((i + 1) * 64)  + (r * 2) + 2
+    E =  (i * 64)       + (l * 2) + 1
+    F =  (i * 64)       + (r * 2) + 3
+    G = ((i + 1) * 64)  + (l * 2) + 1
+    H = ((i + 1) * 64)  + (r * 2) + 3
+    return A, B, C, D, E, F, G, H
+
+def print_triangles(fig, tet):
+    t1 = np.array([ tet[0], tet[1], tet[2], tet[0] ])
+    fig.add_trace(go.Scatter3d(
+        x=t1[:, 0],
+        y=t1[:, 1],
+        z=t1[:, 2]
+    ))
+    
+    t2 = np.array([ tet[0], tet[1], tet[3], tet[0] ])
+    fig.add_trace(go.Scatter3d(
+        x=t2[:, 0],
+        y=t2[:, 1],
+        z=t2[:, 2]
+    ))
+
+    t3 = np.array([ tet[1], tet[2], tet[3], tet[1] ])
+    fig.add_trace(go.Scatter3d(
+        x=t3[:, 0],
+        y=t3[:, 1],
+        z=t3[:, 2]
+    ))
+
+    t4 = np.array([ tet[2], tet[3], tet[0], tet[2] ])
+    fig.add_trace(go.Scatter3d(
+        x=t4[:, 0],
+        y=t4[:, 1],
+        z=t4[:, 2]
+    ))
+
+
 def main() -> None:
     spline = SplineLoader().load('load_spline/spline.txt')
     fig = go.Figure()
@@ -54,15 +105,77 @@ def main() -> None:
     cps = spline.get_control_points()
     fig.add_trace(go.Scatter3d(x=cps[:, 0], y=cps[:, 1], z=cps[:, 2]))
 
+    dps = []
     for i in range(len(cps)):
         cp = cps[i]
-        dps = spline.get_direction_points_from_cp(i)
+        for dp in spline.get_direction_points_from_cp(i):
+            dps.append(dp + cp)
+            dps.append((dp * 1.25) + cp)
 
-        fig.add_trace(go.Scatter3d(
-            x=(dps[:, 0] + cp[0]), 
-            y=(dps[:, 1] + cp[1]), 
-            z=(dps[:, 2] + cp[2])
-        ))
+    cubes = []
+    for i in range(112):
+        cube = []
+        cp = cps[i]
+        for j in range(32 - 1):
+            #    E ----- F  ← (i + 1)
+            #  / |     / |
+            # A ----- B  |  ← (i + 1)
+            # |  G ---|- H  ← i
+            # | /     | /
+            # C ----- D     ← i
+            # 
+            # ↑       ↑
+            # l       r
+            A, B, C, D, E, F, G, H = extract_cuboid_index(i, j, j)
+
+            tet1 = np.array([ dps[A], dps[C], dps[D], dps[G] ])
+            tet2 = np.array([ dps[A], dps[D], dps[F], dps[G] ])
+            tet3 = np.array([ dps[D], dps[F], dps[G], dps[H] ])
+            tet4 = np.array([ dps[A], dps[E], dps[F], dps[G] ])
+            tet5 = np.array([ dps[A], dps[B], dps[D], dps[F] ])
+            
+            print_triangles(fig, tet1)
+            print_triangles(fig, tet2)
+            print_triangles(fig, tet3)
+            print_triangles(fig, tet4)
+            print_triangles(fig, tet5)
+
+            #cube = np.array([ 
+            #    dps[A], dps[B], dps[C], dps[D],
+            #    dps[E], dps[F], dps[G], dps[H]
+            #])
+            #cubes.append(cube)
+            #fig.add_trace(go.Scatter3d(
+            #    x=cube[:, 0],
+            #    y=cube[:, 1],
+            #    z=cube[:, 2]
+            #))
+
+        A, B, C, D, E, F, G, H = extract_cuboid_index(i, 0, 30)
+
+        tet1 = np.array([ dps[A], dps[C], dps[D], dps[G] ])
+        tet2 = np.array([ dps[A], dps[D], dps[F], dps[G] ])
+        tet3 = np.array([ dps[D], dps[F], dps[G], dps[H] ])
+        tet4 = np.array([ dps[A], dps[E], dps[F], dps[G] ])
+        tet5 = np.array([ dps[A], dps[B], dps[D], dps[F] ])
+        
+        print_triangles(fig, tet1)
+        print_triangles(fig, tet2)
+        print_triangles(fig, tet3)
+        print_triangles(fig, tet4)
+        print_triangles(fig, tet5)
+        # A, B, C, D, E, F, G, H = extract_cuboid_index(i, 0, 30)
+        # cube = np.array([ 
+        #     dps[A], dps[B], dps[C], dps[D],
+        #     dps[E], dps[F], dps[G], dps[H]
+        # ])
+        # fig.add_trace(go.Scatter3d(
+        #     x=cube[:, 0],
+        #     y=cube[:, 1],
+        #     z=cube[:, 2]
+        # ))
+        # cubes.append(cube)
+
 
     fig.write_html('output.html')
 
